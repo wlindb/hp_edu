c07a3d533502c106f7b19df89113be83f344edb2
wlindb <wille.lindblom@gmail.com>
2021-05-24 13:14:24 +0200

    Implemented stopwatch

--- client/src/components/StopWatch/StopWatch.js
+++ client/src/components/StopWatch/StopWatch.js
@@ -1,72 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import StopwatchDisplay from './StopWatchDisplay';
+import React, { useState, useRef } from 'react';
 
 const StopWatch = () => {
-    
-    const [isRunning, setIsRunning ] = useState(false);
-    const [currentTimeMin, setCurrentTimeMin] = useState(0);
-    const [currentTimeMs, setCurrentTimeMs] = useState(0);
-    const [currentTimeSec, setCurrentTimeSec] = useState(0);
-
-    useEffect(() => {
-        if(!isRunning) return;
-
-        const interval = setInterval(() => {
-            setCurrentTimeSec(currentTimeSec+1)
-            console.log(currentTimeSec)
-        }, 1000);
-        // return () => clearInterval(interval);
-    }, [isRunning]);
+    const [timer, setTimer] = useState({
+        minutes: 0,
+        seconds: 0
+    });
+    const [isRunning, setIsRunning] = useState(false);
+    const intervalRef = useRef();
 
     const formatTime = val => {
         let value = val.toString();
         if (value.length < 2) {
           value = '0' + value;
         }
-        // if (rest[0] === 'ms' && value.length < 3) {
-        //   value = '0' + value;
-        // }
         return value;
     };
     
     const start = () => {
-        if (!isRunning) {
-          setIsRunning(true);
-        }
+        setIsRunning(true);
+        intervalRef.current = setInterval(() => {
+            setTimer(prevTimer => {
+                const nextSecond = (prevTimer.seconds+1)%60;
+                if(nextSecond === 0) {
+                    return {minutes: (prevTimer.minutes+1)%60, seconds: nextSecond}
+                }
+                return {minutes: prevTimer.minutes, seconds: nextSecond}
+            });
+        }, 1000);
     };
     
     const stop = () => {
         setIsRunning(false);
-        // clearInterval(watch);
-    };
-    
-    const pace = () => {
-        setCurrentTimeSec(currentTimeSec+1);
-        if (currentTimeSec >= 60) {
-            setCurrentTimeMin(currentTimeMin+1);
-            setCurrentTimeSec(0);
-        }
-        console.log(currentTimeMin, ":", currentTimeSec, ":", currentTimeMs)
+        clearInterval(intervalRef.current);
     };
     
     const reset = () => {
-        setCurrentTimeMs(0);
-        setCurrentTimeSec(0);
-        setCurrentTimeMin(0);
+        setTimer({minutes: 0, seconds: 0});
     };
     
 
     return (
         <div className="stopwatch">
-            <h2>Stopwatch</h2>
-            {!isRunning ? <button onClick={start}>START</button> : <button onClick={stop}>STOP</button>}
-            <button onClick={reset}>RESET</button>
-            <div className={"stopwatch__display"}>
-                <span>
-                    {formatTime(currentTimeMin)}:
-                    {formatTime(currentTimeSec)}
-                </span>
-            </div>
+            <h2>{formatTime(timer.minutes)} : {formatTime(timer.seconds)}</h2>
+            {isRunning ? <button onClick={stop}>Stop</button> : <button onClick={start}>Start</button>}
+            <button onClick={reset}>Reset</button>
         </div>
     )
 }
