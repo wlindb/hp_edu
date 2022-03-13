53127412a5a35dec0db99079ca749a80ce1b910b
William <wlindb@kth.se>
2022-03-13 16:26:00 +0100

    WIP: Landing page

--- client/src/components/LandingPage/landing.js
+++ client/src/components/LandingPage/landing.js
@@ -1,10 +1,101 @@
 import React from 'react'
+import Particles from 'react-tsparticles';
 
 const Landing = () => {
+    const particlesInit = (main) => {};
+    
+    const particlesLoaded = (container) => {};
+
+    const particleOptions = {
+        // background: {
+        //   color: {
+        //     value: "#0d47a1",
+        //   },
+        // },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 2,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#a2a2a2",
+          },
+          links: {
+            color: "#a2a2a2",
+            distance: 250,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 40,
+          },
+          opacity: {
+            value: 0.1,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      };
+
     return (
-        <div>
-            Hello from landing page
+      <>
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={particleOptions}
+        />
+        <div style={{height: '400px', width: '800px', marginTop: '200px'}}>
+            <h1 style={{color: 'solid red'}}>Hello from landing page</h1>
         </div>
+      </>
     )
 }
 
